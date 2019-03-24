4358a721ce050f6367a34e6635a7db89efd97499
KellyGoosen1 <47174816+KellyGoosen1@users.noreply.github.com>
2019-03-24 13:57:03 +0000

    Add files via upload
    Preis et al fully simulated in R

--- Working Code.R
+++ Working Code.R
@@ -1,12 +1,23 @@
-# To do:  1. Simulate a number of LIMIT orders in each time period (depending on N_a and alpha)
+
+# To do: CALIBRATION using Lux SMC
+
+# Done: 1. Initialized LOB
+      # 2. Simulated LOB for 250 time steps
+
+
+# Steps taken to achieve this:
+      # A) SIMULATED PRICE of LOs:
+          # 1. q_taker (Mean reverting random walk - depends on delta_s)
+          # 2. lambda(t) (depends on q_taker (MC simulations and current), C_lam and lambda0)
+          # 3. nu(t) (exponetial lambda(t) R.V.)
+          # 4. The Limit Order Book (Buy and Ask side with pb and pa = best bid and ask, respectively)
+
+      # B) SIMULATED QUANTITY of LOs and MOs 
+        # 1. Simulate a number of LIMIT orders in each time period (depending on N_a and alpha)
         # 2. Then, Initialize the order book for 10 MC steps with only LOs hitting the LOB
         # 3. Execute MOs
 
-# Done: Thus far, I have simulated the PRICE of limit buy and ask orders which depend on 
-        # 1. q_taker (Mean reverting random walk - depends on delta_s)
-        # 2. lambda(t) (depends on q_taker (MC simulations and current), C_lam and lambda0)
-        # 3. nu(t) (exponetial lambda(t) R.V.)
-        # 4. The Limit Order Book (Buy and Ask side with pb and pa = best bid and ask, respectively)
+      # C) Simulated Entire Order Book for 250 time steps
 
 
 ###################################################################
@@ -88,15 +99,21 @@ Monte_Carlo_q_taker = function(num_steps = 10^5, TimeHorizon, mean_q, q_taker0, 
   return(mean(MC_q))
 }
 
-lambda0 = 100 # initial value of lambda
-C_lam = 10
-lambda = lambda0
+lambda0 = 0.7 # initial value of lambda
+C_lam = 0.15
 
-for (k in 1:((TimeHorizon+1)){
-  mean_MC_q = Monte_Carlo_q_taker(num_steps=10^5, TimeHorizon=1, mean_q, q_taker0, delta_s)
-  lambda[k+1] = lambda0*(1 + abs(q_taker[k+1]-0.5)/sqrt(mean_MC_q) * C_lam)
+lambda_generate = function(lambda0, C_lam, mean_q, q_taker0, delta_s, Num_orders){
+  lambda = lambda0
+  q_taker = q_taker_evolve(TimeHorizon = Num_orders, mean_q, q_taker0, delta_s)
+
+  for (k in 1:((Num_orders+1))){
+    mean_MC_q = Monte_Carlo_q_taker(num_steps=10^5, TimeHorizon=1, mean_q, q_taker0, delta_s)
+    lambda[k+1] = lambda0*(1 + abs(q_taker[k+1]-0.5)/sqrt(mean_MC_q) * C_lam)
+  }
+  return(lambda)
 }
 
+lambda = lambda_generate(lambda0, C_lam, mean_q, q_taker0, delta_s, TimeHorizon)
 plot(lambda, type="l")
 
 # now we want to simulate nu: exponentially distributed random number
@@ -116,23 +133,30 @@ plot(nu, type="l")
 ###################################################
 # Create a limit buy and limit sell order book
 # create a limit buy order book
-p0 = 100;pb = p0; pa = p0
-
-Limit_Buy_init = as.data.frame(c(pb))
-colnames(Limit_Buy_init) = "price"
 
 library(dplyr)
-Limit_Buy_Book = Limit_Buy_init%>%
-  group_by(price) %>%
-  summarise(count = n())
+create_LOB_0 = function(){ # requires dplyr
+  p0 = 100; pb = p0; pa = p0
 
-Limit_Ask_init = as.data.frame(c(pa))
-colnames(Limit_Ask_init) = "price"
+  Limit_Buy_init = as.data.frame(c(pb))
+  colnames(Limit_Buy_init) = "price"
 
-library(dplyr)
-Limit_Ask_Book = Limit_Ask_init%>%
-  group_by(price) %>%
-  summarise(count = n())
+  Limit_Buy_Book = Limit_Buy_init%>%
+    group_by(price) %>%
+    summarise(count = n())
+
+  Limit_Ask_init = as.data.frame(c(pa))
+  colnames(Limit_Ask_init) = "price"
+
+  Limit_Ask_Book = Limit_Ask_init%>%
+    group_by(price) %>%
+    summarise(count = n())
+
+  myLO_Books = list(Limit_Buy_Book, Limit_Buy_Book)
+  
+  return(myLO_Books)
+}
+LOB_0 = create_LOB_0()
 #######################################
 
 # function to determine the price of the limit order 
@@ -140,21 +164,22 @@ Limit_Ask_Book = Limit_Ask_init%>%
 # and given current nu: defined nu_k
 
 q_provider = 0.5
+nu_k = nu_for_price(1, lambda)
 
 update_LOB_aftereach_LO = function(q_provider, myLO_Books, nu_k){
   Limit_Buy_Book = myLO_Books[[1]]
   Limit_Ask_Book = myLO_Books[[2]]
   uniform = runif(1, 0,1)
   if (uniform < q_provider){ # we have a buy order
-    LO_price = Limit_Ask_Book[1,1][[1]] - 1 - nu_k
+    LO_price = round(Limit_Ask_Book[1,1][[1]] - 1 - nu_k, 2) # round to 2 cents
     LOs = rep(Limit_Buy_Book$price, Limit_Buy_Book$count)
     Limit_Buy_Book = as.data.frame(c(LOs, LO_price))
     colnames(Limit_Buy_Book) = "price"
     Limit_Buy_Book = Limit_Buy_Book%>%
       group_by(price) %>%
       summarise(count = n()) 
-  } else{
-    LO_price = Limit_Buy_Book[nrow(Limit_Buy_Book),1][[1]] - 1 - nu_k
+  } else{ # we have a limit order
+    LO_price = round(Limit_Buy_Book[nrow(Limit_Buy_Book),1][[1]] - 1 - nu_k, 2) # round to cents
     LOs = rep(Limit_Ask_Book$price, Limit_Ask_Book$count)
     Limit_Ask_Book = as.data.frame(c(LOs, LO_price))
     colnames(Limit_Ask_Book) = "price"
@@ -171,13 +196,121 @@ myLO_Books = update_LOB_aftereach_LO(q_provider, myLO_Books, nu_k= 0.5)
 
 # LIMIT ORDER FREQUENCYs
 
+Na = 250 # Number of liquidity providers
+alpha = 0.15 # frequency of MOs or LOs
 
-# Need to create the LOB using 10 MC steps where only LOs can be placed
+Num_orders = floor(Na*alpha)
 
+#################################################################################
 
+# Need to create/initialize the LOB using 10 MC steps where only LOs can be placed
 
+init_LOB = function(Na, alpha, LOB_0, q_provider, lambda0, C_lam, mean_q, q_taker0, delta_s){
+  # initialize: Fill the order book before the commencement of actual trading.
+  # generate and insert initial limit orders for 10 
+  # Monte Carlo steps
+  # The number of orders inserted depends on the number of traders, N_A, and frequency of limit orders, alpha, stored in simRun.
+  # After the completion of initialization, the modified PreisModel
+  # object is returned.
+  
+  # Generate Initial Orders for 10 MC steps
+  num_LOs = 10*floor(Na*alpha) # 10 x number of LOs in each MC step
+  
+  # Generate lambda to obtain nu for each LO
+  lambda = lambda_generate(lambda0, C_lam, mean_q, q_taker0,delta_s, Num_orders = num_LOs) # SLOW
+  nu_k = nu_for_price(TimeHorizon = num_LOs, lambda)
+  
+  # For each LO, update LOB using nu and best ask and best bid
+  myLO_Books = LOB_0
+  for (i in 1:num_LOs){
+    myLO_Books = update_LOB_aftereach_LO(q_provider, myLO_Books, nu_k = nu_k[i])
+  }
+  return(myLO_Books)
+}
 
+LOB_0 = create_LOB_0()
+myLO_Books = init_LOB(Na, alpha, LOB_0, q_provider, lambda0, C_lam, mean_q, q_taker0, delta_s)
+myLOB_init = myLO_Books
 ########################################################################
 
+# Next we need a function that executes a single MO
+# for each MC step there are [mu*Na] MOs
+# probability MO = buy order = q_taker (which is not fixed - changes each MC step
+# However, qtaker is fixed for all MOs in each time step --> qtaker(t)
+
+q_taker_k = q_taker[10]
+# Function to execute a single MO
+execute_single_MO = function(q_taker_k, myLO_Books){
+  u = runif(1, min=0, max=1)
+  if (u < q_taker_k){ # market buy order hits best ask
+    myLO_Books[[2]][1,2] = myLO_Books[[2]][1,2] - 1 # subtract 1 from count of best ask
+    myLO_Books[[2]] = myLO_Books[[2]][myLO_Books[[2]]$count !=0, ] # get rid of rows with 0 count
+  }else{ # market sell order hits best bid
+    myLO_Books[[1]][1,2] = myLO_Books[[1]][1,2] - 1 # subtract 1 from count of best ask
+    myLO_Books[[1]] = myLO_Books[[1]][myLO_Books[[1]]$count !=0, ] # get rid of rows with 0 count
+  }
+  return(myLO_Books)
+}
+
+mu = 0.0067
+# function to execute all MOs in a single time step --> [Na*mu] MOs
+execute_MOs_1timestep = function(myLO_Books, q_taker_k, Na, mu){
+  num_MOs = floor(Na*mu)
+  for (k in 1:num_MOs){
+    myLO_Books = execute_single_MO(q_taker_k, myLO_Books)
+  }
+  return(myLO_Books)
+}
+
+myLO_Books= execute_MOs_1timestep(myLO_Books, q_taker_k, Na, mu)
+
+####################################################################
+
+# Now I have a function which executes MOs each time step and
+# a function which executes single LOs
+
+# So, we need a function which excutes [Na*alpha] LOs FIRST
+# then the MOs 
+# in each timestep
+# and then returns the new LOB
+
+LO_MO_1timestep = function(Na, alpha,mu, myLO_Books, q_provider,
+                           lambda_t, nu_t, q_taker_t){
+  # 1. LOs hit LOB
+  num_LOs = floor(Na*alpha) # number of LOs
+  for (k in 1:num_LOs){ # update LOB for each LO in the time step
+    myLO_Books = update_LOB_aftereach_LO(q_provider, myLO_Books, nu_t)
+  }
+  
+  # 2. MOs hit LOB
+  myLO_Books = execute_MOs_1timestep(myLO_Books, q_taker_t, Na, mu)
+  
+  return(myLO_Books)
+}
 
 
+
+######################################################################
+
+# Now that we have an algorithm which executes all LOs and then MOs in a SINGLE TIME STEP
+# we need a function that executes all LOs and MO
+
+Sim_OB = function(lamda0, C_lam, mean_q, q_taker0, delta_s, TimeHorizon, Na, alpha, mu, myLOB_init, q_provider){
+  lambda = lambda_generate(lambda0, C_lam, mean_q, q_taker0, delta_s, Num_orders = TimeHorizon)
+  nu = nu_for_price(TimeHorizon = TimeHorizon, lambda)
+  q_taker = q_taker_evolve(TimeHorizon, mean_q, q_taker0, delta_s)
+  myLO_Books = myLOB_init
+  
+  for (t in 1:TimeHorizon){
+    lambda_t = lambda[t]; nu_t = nu[t]; q_taker_t = q_taker[t]
+    myLO_Books = LO_MO_1timestep(Na, alpha, mu, myLO_Books, q_provider,
+                                            lambda_t, nu_t, q_taker_t)
+  }
+  
+  return(myLO_Books)
+}
+
+
+simLOB = Sim_OB(lamda0, C_lam, mean_q, q_taker0, delta_s, TimeHorizon=250, Na, alpha, mu, myLOB_init, q_provider)
+
+####################################################################
